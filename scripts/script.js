0b1e00b3724971a1adb2a3d3f1a187af1e9aaa31
emre <emreozturanli1@gmail.com>
2022-06-20 16:54:45 +0300

    subscribe adjusment

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
-/* function sendMail(){
-   
-    let params = {email_id : document.getElementById('email').value};
-    emailjs.send("service_fdwk1oy","template_kxcd0ek",params).then((res) => {
-        console.log(res.status)
-    })
-}  */
+const emailContent = document.querySelector('.email-content');
 
+function sendMail(){
+      
+    if(emailValid(document.getElementById('email').value)){
+      let params = {email_id : document.getElementById('email').value};
+      emailjs.send("service_fdwk1oy","template_kxcd0ek",params).then((res) => {
+      console.log(res.status);
+      emailContent.innerHTML = `<h3 class="title">Thanks For Subscribing</h3>`;
+    })
+    }
+    else{
+      alert('Please type a valid email address!')
+    }
+    
+}  
+
+function emailValid(email){
+  let pattern=/^\w+@[a-z]+\.([a-z]{2,3})$/
+   // return email.match(pattern) ? true:false
+   return pattern.test(email)
+}
 
 let list = document.querySelectorAll('.list');
 let itemBox = document.querySelectorAll('.unit');
