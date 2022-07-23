43cd1cff2cffe8b19db799395f1297c12051765e
celal <celalyasinnari@gmail.com>
2022-07-23 18:11:35 +0300

    a lot of changes

--- insert.php
+++ insert.php
@@ -1,50 +1,20 @@
-<?php
-if (isset($_POST['submit'])) {
-    if (isset($_POST['email'])) {
-        
-        $email = $_POST['email'];
-        $host = "db5008944633.hosting-data.io";
-        $dbUsername = "dbu2095089";
-        $dbPassword = "Try2022Harder!!";
-        $dbName = "dbs7549006";
-        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-        if ($conn->connect_error) {
-            die('Could not connect to the database.');
-        }
-        else {
-            $Select = "SELECT email FROM subscribers_list WHERE email = ? LIMIT 1";
-            $Insert = "INSERT INTO subscribers_list(email) values(?)";
-            $stmt = $conn->prepare($Select);
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $stmt->bind_result($resultEmail);
-            $stmt->store_result();
-            $stmt->fetch();
-            $rnum = $stmt->num_rows;
-            if ($rnum == 0) {
-                $stmt->close();
-                $stmt = $conn->prepare($Insert);
-                $stmt->bind_param("s",$email);
-                if ($stmt->execute()) {
-                    echo "New record inserted sucessfully.";
-                }
-                else {
-                    echo $stmt->error;
-                }
-            }
-            else {
-                echo "Someone already registers using this email.";
-            }
-            $stmt->close();
-            $conn->close();
-        }
-    }
-    else {
-        echo "All field are required.";
-        die();
-    }
+<?php 
+
+$server= "db5008944633.hosting-data.io"; /* Address of the IONOS Database servers */
+$user= "dbu2095089"; /* Database Username */
+$password= "Try2022Harder!!"; /* Password */
+$database= "dbs7549006"; /* Name of the Database */
+/* Access SQL Server and create the table */
+
+$link = mysqli_connect($server, $user, $password, $database);
+
+if (isset($_POST["submit"]))
+{
+	$email = $_POST['email'];
+  	$add = "INSERT INTO subscribers_list (email) VALUES ('$email')";
+  	$run = mysqli_query($link,$add);
 }
-else {
-    echo "Submit button is not set";
-}
+ 
+mysqli_close($link);
+   
 ?>
